2fdca7e4eb1c5bbe2c4562414c48b9b14510c2d4
Eugene Hauptmann <eugene@reactivelions.com>
2024-01-14 15:20:20 -0500

    added devtools

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager as _;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,6 +12,14 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            // #[cfg(debug_assertions)] // only include this code on debug builds
+            // {
+              let window = app.get_window("main").unwrap();
+              window.open_devtools();
+            // }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
